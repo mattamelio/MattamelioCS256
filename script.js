4d93427a40ad82d4b8681474bb2e9a9ff82b6553
matt amelio <matthewamelio@kings.edu>
2021-10-11 16:03:08 -0400

    hamburger

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const hamburger = document.querySelector(".hamburger"); // the hamburger button
+const navMenu = document.querySelector(".nav-menu");    // the navigation menu
+const navLink = document.querySelectorAll(".nav-link"); // array of all of the links in the nav menu
+
+// Add an event listener so the hamburger calls openMenu when it is clicked
+
+// Your code here!
+
+
+
+
+// function to open the navigation menu
+// this needs to add/remove the class "active" for both the hamburger and navMenu
+// elements so that the hamburger will switch to an X and the navMenu will
+// slide out, or the hamburger will go back to a burger and the menu will slide in
+function toggleActive() {
+    // Your code here!
+}
+
+
+
+// Add an event listener FOR EACH navigation link so the menu will close
+// when it is clicked
+
+// Your code here!
+
+
+
+
+// function to close the navigation nav
+// this needs to remove the class "active" from both hamburger and navMenu
+function closeMenu() {
+
+  // Your code here!
+}
