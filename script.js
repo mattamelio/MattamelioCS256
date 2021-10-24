57a93d486091e004366aa056124dd39b6bd71d6c
matt amelio <matthewamelio@kings.edu>
2021-10-24 18:32:51 -0400

    darkmode

--- script.js
+++ script.js
@@ -2,43 +2,61 @@
 const hamburger = document.querySelector(".hamburger"); // the hamburger button
 const navMenu = document.querySelector(".nav-menu");    // the navigation menu
 const navLink = document.querySelectorAll(".nav-link"); // array of all of the links in the nav menu
+const darkmode = document.querySelector("#dark");
+const body = document.getElementByTageName("body")[0];
+const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
+const bar = document.querySelector(".bar");
+
+
 
 // Add an event listener so the hamburger calls openMenu when it is clicked
 
 // Your code here!
 
-hamburger.addEventListener("click", toggleActive());
+hamburger.addEventListener("click", toggleActive);
+darkmode.addEventListener("click", toggleDark);
 
 
 // function to open the navigation menu
 // this needs to add/remove the class "active" for both the hamburger and navMenu
 // elements so that the hamburger will switch to an X and the navMenu will
 // slide out, or the hamburger will go back to a burger and the menu will slide in
+
 function toggleActive() {
-    // Your code here!
 	
-	this.hamburger.toggle("is-active");
-	if (this.hamburger.contains('is-active')){
-		openNav();
-	} else{
-		close.Nav();
-	}
-
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
 }
-
 
 
 // Add an event listener FOR EACH navigation link so the menu will close
 // when it is clicked
 
-// Your code here!
-
+for(var i = 0; i < navLink.length; i++) {
+	
+	navLink[i].addEventListener("click", closeMenu);
+}
 
 
 
 // function to close the navigation nav
 // this needs to remove the class "active" from both hamburger and navMenu
 function closeMenu() {
+	
+	hamburger.classList.remove("active);
+	navMenu.classList.remove("active");
+}
 
-  // Your code here!
+
+
+function toggleDark() {
+	
+	body.classList.toggle("darkmode");
+	header.classList.toggle("darkmode");
+	footer.classLst.toggle("darkmode");
+
+	for(var i = 0; i < bar.length; i++){
+		bar[i].classList.toggle("darkmode");
+	}
 }
